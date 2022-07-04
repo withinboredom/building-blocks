571b2ae13e115a2cf0ec165f219abf061827c598
Robert Landers <landers.robert@gmail.com>
2022-07-04 13:03:58 +0200

    automatically encode exceptions

--- src/Result.php
+++ src/Result.php
@@ -2,19 +2,30 @@
 
 namespace Withinboredom\BuildingBlocks;
 
+use Throwable;
 use Withinboredom\ResponseCode\HttpResponseCode;
 
 class Result
 {
-    public function __construct(public readonly HttpResponseCode $code, public readonly string|null $body = null)
-    {
+    private static int $EXCEPTION_SERIALIZATION_FLAGS = JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES;
+
+    public function __construct(
+        public readonly HttpResponseCode $code,
+        public readonly string|null|Throwable $body = null
+    ) {
+    }
+
+    public static function setExceptionSerializationFlags(int $flags = JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES
+    ): void {
+        self::$EXCEPTION_SERIALIZATION_FLAGS = $flags;
     }
 
     public function emit(): never
     {
         http_response_code($this->code->value);
         if ($this->body !== null) {
-            echo $this->body;
+            echo $this->body instanceof Throwable ? json_encode(['errors' => [$this->body->getMessage()]],
+                self::$EXCEPTION_SERIALIZATION_FLAGS) : $this->body;
         }
         die();
     }
